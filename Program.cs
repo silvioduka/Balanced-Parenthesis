2346e26a70363b4624d7a553ebde5a45255823e7
silvio <silvio.duka@hotmail.com>
2018-08-13 17:14:57 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Balanced Parenthesis - Easy from Coding Challenges
+by Silvio Duka
+
+Last modified date: 2018-03-08
+
+Create a program that checks if in a given string expression all the parenthesis are balanced. 
+For Example: 
+(test) - valid 
+(no() - invalid 
+()(()) - valid 
+(123(456)(7))( - invalid 
+(val()id) - valid 
+
+Also, take into account the "\" escape sequences: 
+(nope\) - invalid 
+(v\(al) - valid 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedParenthesis
+{
+    class Program
+    {
+        static string input = @"(123(456)(7))(v\(al)"; // Insert here a string to check if all the parenthesis are balanced
+
+        static void Main(string[] args)
+        {
+            int p = 0;
+            char o = ' ';
+
+            foreach (char c in input)
+            {
+                if (c == '(' && o != '\\') { p++; continue; }
+                if (c == ')' && o != '\\') { p--; continue; }
+
+                o = c;
+            }
+
+            Console.WriteLine($"Input string: {input}\nParenthesis {((p==0)?"are":"are NOT")} balanced!");
+        }
+    }
+}
